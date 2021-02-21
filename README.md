1037d75d36fb346cf3c499238a2a0e9419d63c9b
Francis Perez <francistp11@gmail.com>
2021-02-21 09:43:16 -0500

    Initial Submission

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# grid-challenge
+# Grid-Challenge
+
--- gridReader.js
+++ gridReader.js
@@ -3,31 +3,27 @@ var fs = require('fs')
 var initialRead = fs.readFileSync('number_array.txt', 'utf8', function(err, data){
     if(err) throw err;
     var numbersArray = Array.from(data)
+    console.log("this is nums array after read", numbersArray)
     return numbersArray
 });
 
+var correctArr = initialRead.split("\r\n")
 function gridReader(array){
     
     var greatestProduct = 0;
     var firstNumbersCord = [];
     var direction = "";
 
-    const cleanArray = array.split(" ").map((item) => {
-        return parseInt(item)
+    const newMatrix = array.map((string) => {
+        return string.split(" ").map((item) => {
+            return parseInt(item)
+        })
     })
-    var newMatrix = []
-
-    function createMatrix(cleanArray){
-        for(let i = 0; i < cleanArray.length; i++){
-            newMatrix.push(cleanArray.splice(0,20))
-        }
-    }
-
-    createMatrix(cleanArray) 
-
+    
     function traverseRight(matrix){
         for(let i = 0; i < matrix.length; i++){
-            for(let j = 0; j < matrix[i].length-4; j++){
+            console.log("this should be row",matrix[i])
+            for(let j = 0; j < matrix[i].length-3; j++){
                 var tempProd = matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3]
                 if(tempProd > greatestProduct){
                     greatestProduct = tempProd
@@ -41,7 +37,7 @@ function gridReader(array){
 
 
     function traverseDown(matrix){
-        for(let i = 0; i < matrix.length-4; i++){
+        for(let i = 0; i < matrix.length-3; i++){
             for(let j = 0; j < matrix[i].length; j++){    
                 var tempProd = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j]
                 if(tempProd > greatestProduct){
@@ -56,8 +52,8 @@ function gridReader(array){
     traverseDown(newMatrix)
 
     function traverseRightDiagonal(matrix){
-        for(let i = 0; i < matrix.length-4; i++){
-            for(let j = 0; j < matrix[i].length-4; j++){
+        for(let i = 0; i < matrix.length-3; i++){
+            for(let j = 0; j < matrix[i].length-3; j++){
                 var tempProd = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3]
                 if(tempProd > greatestProduct){
                     greatestProduct = tempProd
@@ -71,8 +67,8 @@ function gridReader(array){
     traverseRightDiagonal(newMatrix)
 
     function traverseLeftDiagonal(matrix){
-        for(let i = 0; i < matrix.length-4; i++){
-            for(let j = 4; j < matrix[i].length; j++){
+        for(let i = 0; i < matrix.length-3; i++){
+            for(let j = 3; j < matrix[i].length; j++){
                 var tempProd = matrix[i][j] * matrix[i+1][j-1] * matrix[i+2][j-2] * matrix[i+3][j-3]
                 if(tempProd > greatestProduct){
                     greatestProduct = tempProd
@@ -96,7 +92,7 @@ function gridReader(array){
     ]
 }
 
-gridReader(initialRead)
+gridReader(correctArr)
 
 
 
